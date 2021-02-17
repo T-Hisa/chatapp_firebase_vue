de144ca175556b7679898f8f0cc08647cc16d86a
Takaaki <dacchin.h081229@gmail.com>
2021-02-17 18:56:21 +0900

    commit

--- setup-storage.js
+++ setup-storage.js
@@ -1,15 +1,20 @@
 'use strict'
 const admin = require('firebase-admin')
 const path = require('path')
-const serviceAccount = require('./serviceAccount.json')
-const bucketURL = 'gs://chat-app-db732.appspot.com/'
+let project = process.env.PROJECT
+const wholeSettings = require('./settings.js')
+if (!wholeSettings.hasOwnProperty(project)) {
+  project = "default"
+}
+const settings = wholeSettings[project]
+const {serviceAccount} = settings
+const bucketURL = settings.bucketURL
 const app = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: bucketURL
 })
 const bucket = app.storage().bucket()
-const project = process.env.PROJECT
-const fileName = `${project}/settings.js`
+const fileName = 'config/settings.js'
 const fileRef = bucket.file(fileName)
 const localFilePath = path.resolve(__dirname, 'src/config/settings.js')
 
